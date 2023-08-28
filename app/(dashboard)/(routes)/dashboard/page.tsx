13b4a29a10f18a36aec90e5024105c6ee41cb3d4
iam-deepakvel <deepakvel82@gmail.com>
2023-08-28 19:01:18 +0530

    feat: add clerk authentication

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,5 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
 const DashboardPage = () => {
-  return <p>Dashboard Page</p>;
+  return (
+    <>
+      <p>Dashboard Page</p>
+      <UserButton afterSignOutUrl="/" />
+    </>
+  );
 };
 
 export default DashboardPage;
